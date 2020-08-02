cc17983ad88850ef968c03e151a914043ce756c7
Lajcik007 <powalka92@gmail.com>
2020-08-02 20:38:36 +0200

    work

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// TEST API CREATE
+// npx aws-api-gateway-cli-test \
+//   --username='admin@example.com' \
+//   --password='Passw0rd!' \
+//   --user-pool-id='eu-central-1_3EtBs2njA' \
+//   --app-client-id='3iv1kt7r7mp64iruqn453hveg3' \
+//   --cognito-region='eu-central-1' \
+//   --identity-pool-id='eu-central-1:8ab69d4f-1a93-4864-b859-774edb3e7fc7' \
+//   --invoke-url='https://eawnwn1fl3.execute-api.eu-central-1.amazonaws.com/dev/' \
+//   --api-gateway-region='eu-central-1' \
+//   --path-template='notes' \
+//   --method='POST' \
+//   --body='{"content":"hello world","attachment":"hello.jpg"}'
